ff3127186465a68ff3af53f730fcc7219cb7da6b
mooziii <63669478+mooziii@users.noreply.github.com>
2022-05-18 15:39:17 +0200

    Separate init dinger

---
+++ src/main/java/net/fabricmc/example/ClientInit.java
@@ -0,0 +1,13 @@
+package net.fabricmc.example;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientInit implements ClientModInitializer {
+
+    public static double tickSpeed = 0.5;
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/fabricmc/example/ServerInit.java
@@ -0,0 +1,11 @@
+package net.fabricmc.example;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class ServerInit implements DedicatedServerModInitializer {
+
+	@Override
+	public void onInitializeServer() {
+
+	}
+}
